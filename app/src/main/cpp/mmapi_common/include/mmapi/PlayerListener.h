bfe9a5965df5935f93ae9d8ce4d6521e07ff72b8
Verdial <99362544+wakeupver@users.noreply.github.com>
2026-04-04 14:31:10 +0700

    Update PlayerListener.h

--- app/src/main/cpp/mmapi_common/include/mmapi/PlayerListener.h
+++ app/src/main/cpp/mmapi_common/include/mmapi/PlayerListener.h
@@ -1,50 +1,58 @@
-//
-// Created by woesss on 13.08.2023.
-//
-
 #ifndef MMAPI_PLAYER_LISTENER_H
 #define MMAPI_PLAYER_LISTENER_H
 
 #include <jni.h>
+#include <mutex>
+#include <atomic>
+#include <memory>
 
 namespace mmapi {
 
     enum PlayerState {
-        CLOSED = 0,
+        CLOSED     = 0,
         UNREALIZED = 100,
-        REALIZED = 200,
+        REALIZED   = 200,
         PREFETCHED = 300,
-        STARTED = 400,
+        STARTED    = 400,
     };
 
     enum PlayerListenerEvent {
         RESTART = 1,
-        STOP = 2,
-        ERROR = 3,
+        STOP    = 2,
+        ERROR   = 3,
     };
 
     class JNIEnvPtr {
-        bool isJavaThread;
+        bool    isJavaThread;
         JNIEnv *env;
     public:
         JNIEnvPtr();
-        virtual ~JNIEnvPtr();
+        ~JNIEnvPtr();
         JNIEnv *operator->() const;
 
         static JavaVM *vm;
     };
 
     class PlayerListener {
-        jobject listener;
-        jmethodID method;
     public:
         PlayerListener(JNIEnv *env, jobject pListener);
         virtual ~PlayerListener();
 
-        void sendEvent(PlayerListenerEvent eventType, const int64_t time);
         void postEvent(PlayerListenerEvent type, int64_t time);
 
-    };
-} // mmapi
+    private:
+        struct SharedState {
+            std::mutex          guard;
+            std::atomic<bool>   closed{false};
+            jobject             listener{nullptr};
+            jmethodID           method{nullptr};
+        };
 
-#endif //MMAPI_PLAYER_LISTENER_H
+        std::shared_ptr<SharedState> state;
+
+        void sendEvent(PlayerListenerEvent type, int64_t time);
+    };
+
+} // namespace mmapi
+
+#endif
