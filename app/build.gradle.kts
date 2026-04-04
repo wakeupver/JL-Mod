e0aacfb98acb4072b233c3db63a688e4b08d6409
Verdial <99362544+wakeupver@users.noreply.github.com>
2026-04-04 12:01:11 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         buildConfig = true
     }
 
-    signingConfigs.create("KEYSTORE") {
+    signingConfigs.create("emulator") {
         if (secret.isNotEmpty()) {
             keyAlias = secret.getProperty("keyAlias")
             keyPassword = secret.getProperty("keyPassword")
